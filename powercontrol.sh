89ca49db1028e71ec6632a8b694677fcb73fee70
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2020-09-15 14:57:10 +0530

    Remove echo output lines

--- powercontrol.sh
+++ powercontrol.sh
@@ -6,23 +6,11 @@
 # Built using information from the Arch Wiki
 # https://wiki.archlinux.org/index.php/Lenovo_IdeaPad_5_14are05#Tips_and_tricks
 
-# Check if acpi_call is loaded
-if ! lsmod | grep -q acpi_call; then
-    echo "Error: acpi_call module not loaded!" >&2
-    exit 1
-fi
-
-# Check if root
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must run as root!" >&2
-    exit 1
-fi
-
 toggle_batteryconserve() {
     if [ "$batteryconserve" = "On" ]; then
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x05' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x05' >/proc/acpi/call
     elif [ "$batteryconserve" = "Off" ]; then
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x03' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x03' >/proc/acpi/call
     else
         echo 'Something went wrong, exiting :('
         exit 0
@@ -34,9 +22,9 @@ toggle_batteryconserve() {
 
 toggle_rapidcharge() {
     if [ "$rapidcharge" = "On" ]; then
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x08' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x08' >/proc/acpi/call
     elif [ "$rapidcharge" = "Off" ]; then
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x07' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.SBMC 0x07' >/proc/acpi/call
     else
         echo 'Something went wrong, exiting :('
         exit 0
@@ -47,7 +35,7 @@ toggle_rapidcharge() {
 }
 
 get_batteryconserve() {
-    echo '\_SB.PCI0.LPC0.EC0.BTSG' | sudo tee /proc/acpi/call
+    echo '\_SB.PCI0.LPC0.EC0.BTSG' >/proc/acpi/call
     btsg=$(sudo cat /proc/acpi/call | cut -d '' -f1)
 
     if [ "$btsg" = 0x0 ]; then
@@ -60,7 +48,7 @@ get_batteryconserve() {
 }
 
 get_rapidcharge() {
-    echo '\_SB.PCI0.LPC0.EC0.FCGM' | sudo tee /proc/acpi/call
+    echo '\_SB.PCI0.LPC0.EC0.FCGM' >/proc/acpi/call
     fcgm=$(sudo cat /proc/acpi/call | cut -d '' -f1)
 
     if [ "$fcgm" = 0x0 ]; then
@@ -73,10 +61,10 @@ get_rapidcharge() {
 }
 
 get_mode() {
-    echo '\_SB.PCI0.LPC0.EC0.STMD' | sudo tee /proc/acpi/call
+    echo '\_SB.PCI0.LPC0.EC0.STMD' >/proc/acpi/call
     stmd=$(sudo cat /proc/acpi/call | cut -d '' -f1)
 
-    echo '\_SB.PCI0.LPC0.EC0.QTMD' | sudo tee /proc/acpi/call
+    echo '\_SB.PCI0.LPC0.EC0.QTMD' >/proc/acpi/call
     qtmd=$(sudo cat /proc/acpi/call | cut -d '' -f1)
 
     # echo 'qtmd:' "$qtmd" 'stmd:' "$stmd"
@@ -95,13 +83,13 @@ get_mode() {
 switch_mode() {
     case $mode_val in
     1)
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0013B001' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0013B001' >/proc/acpi/call
         ;;
     2)
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x000FB001' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x000FB001' >/proc/acpi/call
         ;;
     3)
-        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0012B001' | sudo tee /proc/acpi/call
+        echo '\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0012B001' >/proc/acpi/call
         ;;
     *)
         echo "Mode $mode_val is invalid!" >&2
@@ -148,6 +136,18 @@ usage() {
 # PowerControl start
 #
 # ------------------
+
+# Check if acpi_call is loaded
+if ! lsmod | grep -q acpi_call; then
+    echo "Error: acpi_call module not loaded!" >&2
+    exit 1
+fi
+
+# Check if root
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must run as root!" >&2
+    exit 1
+fi
 
 # Display usage if no args are supplied
 if [ -z "$1" ]; then
